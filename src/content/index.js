533369a4a7edf94960256d27d1a786ffceb3e688
Mustafa Sancu <mustafa.sancu@demirorenteknoloji.com>
2020-07-30 19:48:54 +0300

    double button check

--- src/content/index.js
+++ src/content/index.js
@@ -7,30 +7,32 @@ const observer = new Observer();
 const downloader = new Downloader();
 
 observer.observe((tweet) => {
-  tweet.$el.classList.add("button-group");
-  const { clientWidth, clientHeight } = tweet.$el.querySelector("svg");
+  if (!tweet.$el.classList.contains("button-group")) {
+    tweet.$el.classList.add("button-group");
+    const { clientWidth, clientHeight } = tweet.$el.querySelector("svg");
 
-  const button = document.createElement("button");
-  button.addEventListener("click", () => {
-    button.classList.add("loading");
-    chrome.runtime.sendMessage(
-      { action: "getVideoUrl", id: tweet.id },
-      async (response) => {
-        if (response && response.status) {
-          await downloader.download(response.url, response.name);
+    const button = document.createElement("button");
+    button.addEventListener("click", () => {
+      button.classList.add("loading");
+      chrome.runtime.sendMessage(
+        { action: "getVideoUrl", id: tweet.id },
+        async (response) => {
+          if (response && response.status) {
+            await downloader.download(response.url, response.name);
+          }
+          button.classList.remove("loading");
         }
-        button.classList.remove("loading");
-      }
+      );
+    });
+
+    button.insertAdjacentHTML(
+      "beforeend",
+      Mustache.render(Button, {
+        clientWidth,
+        clientHeight,
+      })
     );
-  });
 
-  button.insertAdjacentHTML(
-    "beforeend",
-    Mustache.render(Button, {
-      clientWidth,
-      clientHeight,
-    })
-  );
-
-  tweet.$el.appendChild(button);
+    tweet.$el.appendChild(button);
+  }
 });
