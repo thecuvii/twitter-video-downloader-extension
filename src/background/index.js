c497cb0abd27823965ce76cc1bd50c99e21a95e6
mustafa <mstfsnc@gmail.com>
2024-03-04 12:05:31 +0300

    x host added

--- src/background/index.js
+++ src/background/index.js
@@ -2,7 +2,7 @@ import browser from "webextension-polyfill";
 
 browser.runtime.onInstalled.addListener(function () {
   browser.tabs
-    .query({ url: "*://twitter.com/*", currentWindow: true })
+    .query({ url: ["*://twitter.com/*", "*://x.com/*"], currentWindow: true })
     .then(function (tabs) {
       tabs.forEach(function (tab) {
         browser.tabs.reload(tab.id);
